839b51c37ee555ca9c8ddfae67bf8a574d4102a9
Luís Henrique <carvalholuis839@gmail.com>
2024-12-05 19:49:51 -0300

    Update jogo.c.c

--- jogo.c.c
+++ jogo.c.c
@@ -8,7 +8,6 @@
 #define LARGURA_MAPA 12
 #define ALTURA_MAPA 8
 
-
 int mapa[ALTURA_MAPA][LARGURA_MAPA];
 
 Jogo* inicializar_jogo() {
@@ -22,12 +21,11 @@ Jogo* inicializar_jogo() {
     jogo->sprite = al_load_bitmap("sprites.png");
     jogo->chao = al_load_bitmap("areia.png");
     jogo->parede = al_load_bitmap("pedra.png");
-    jogo->agua = al_load_bitmap ("agua2.png");
-    jogo->cenario_1 = al_load_bitmap ("caveira.png");
-    jogo->cenario_2 = al_load_bitmap ("cacto.png");
+    jogo->agua = al_load_bitmap("agua2.png");
+    jogo->cenario_1 = al_load_bitmap("caveira.png");
+    jogo->cenario_2 = al_load_bitmap("cacto.png");
 
-
-    if (!jogo->sprite || !jogo->chao || !jogo->parede || !jogo->agua ) {
+    if (!jogo->sprite || !jogo->chao || !jogo->parede || !jogo->agua) {
         printf("Erro ao carregar uma das imagens\n");
         exit(1);
     }
@@ -149,100 +147,25 @@ void atualizar_jogo(Jogo* jogo) {
     }
 }
 
-
-void processar_entrada(Jogo* jogo, ALLEGRO_EVENT event) {
-    if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
-        jogo->keys[event.keyboard.keycode] = true;
-    } else if (event.type == ALLEGRO_EVENT_KEY_UP) {
-        jogo->keys[event.keyboard.keycode] = false;
-    }
-}
-
-void atualizar_jogo(Jogo* jogo) {
-    static int anim_counter = 0;
-
-    int novo_x = jogo->x;
-    int novo_y = jogo->y;
-
-    // Tentativa de mover o personagem
-    if (jogo->keys[ALLEGRO_KEY_RIGHT]) {
-        novo_x += 14;
-        jogo->direcao = 3;
-    } else if (jogo->keys[ALLEGRO_KEY_LEFT]) {
-        novo_x -= 14;
-        jogo->direcao = 1;
-    } else if (jogo->keys[ALLEGRO_KEY_DOWN]) {
-        novo_y += 14;
-        jogo->direcao = 0;
-    } else if (jogo->keys[ALLEGRO_KEY_UP]) {
-        novo_y -= 14;
-        jogo->direcao = 2;
-    }
-
-    // Margens da área de colisão (ajuste os valores para afinar o resultado)
-    const int margem_esquerda = 40;
-    const int margem_direita = 40;
-    const int margem_superior = 30;
-    const int margem_inferior = 30;
-
-    // Dimensões do personagem
-    const int largura_personagem = 94;  // Largura total do sprite
-    const int altura_personagem = 99;  // Altura total do sprite
-
-    // Verifica se a nova posição está dentro de uma área válida
-    if (
-        verificar_posicao_valida(novo_x + margem_esquerda, novo_y + margem_superior) &&
-        verificar_posicao_valida(novo_x + largura_personagem - margem_direita, novo_y + margem_superior) &&
-        verificar_posicao_valida(novo_x + margem_esquerda, novo_y + altura_personagem - margem_inferior) &&
-        verificar_posicao_valida(novo_x + largura_personagem - margem_direita, novo_y + altura_personagem - margem_inferior)
-    ) {
-        jogo->x = novo_x;
-        jogo->y = novo_y;
-    }
-
-    // Atualiza a animação do sprite
-    if (jogo->keys[ALLEGRO_KEY_RIGHT] || jogo->keys[ALLEGRO_KEY_LEFT] ||
-        jogo->keys[ALLEGRO_KEY_UP] || jogo->keys[ALLEGRO_KEY_DOWN]) {
-        anim_counter++;
-        if (anim_counter % 2 == 0) {
-            jogo->si = (jogo->si + 1) % 3;
-        }
-    } else {
-        jogo->si = 1;
-    }
-}
-
-
 void renderizar_jogo(Jogo* jogo) {
-
     for (int i = 0; i < ALTURA_MAPA; i++) {
-    for (int j = 0; j < LARGURA_MAPA; j++) {
-        printf("%d ", mapa[i][j]);
+        for (int j = 0; j < LARGURA_MAPA; j++) {
+            al_draw_bitmap(jogo->chao, j * 70, i * 70, 0);
+
+            if (mapa[i][j] == 1) {
+                al_draw_bitmap(jogo->parede, j * 70, i * 70, 0);
+            }
+            if (mapa[i][j] == 2) {
+                al_draw_bitmap(jogo->agua, j * 70, i * 70, 0);
+            }
+            if (mapa[i][j] == 3) {
+                al_draw_bitmap(jogo->cenario_1, j * 70, i * 70, 0);
+            }
+            if (mapa[i][j] == 4) {
+                al_draw_bitmap(jogo->cenario_2, j * 70, i * 70, 0);
+            }
+        }
     }
-    printf("\n");
-}
-    // desenha o cenario
-for (int i = 0; i < ALTURA_MAPA; i++) {
-    for (int j = 0; j < LARGURA_MAPA; j++) {
-        al_draw_bitmap(jogo->chao , j * 70, i * 70, 0);
-
-        if (mapa[i][j] == 1) {
-            al_draw_bitmap(jogo->parede, j * 70, i * 70, 0);
-        }
-        if (mapa[i][j] == 2) {
-            al_draw_bitmap(jogo->agua, j * 70, i * 70, 0);
-        }
-        if (mapa[i][j] == 3) {
-            al_draw_bitmap(jogo->cenario_1, j * 70, i * 70, 0);
-        }
-        if (mapa[i][j] == 4) {
-            al_draw_bitmap(jogo->cenario_2, j * 70, i * 70, 0);
-        }
-
-
-    }
-}
-
 
     // desenha o personagem
     int flip = 0;
@@ -268,7 +191,6 @@ void finalizar_jogo(Jogo* jogo) {
     al_destroy_bitmap(jogo->sprite);
     al_destroy_bitmap(jogo->chao);
     al_destroy_bitmap(jogo->parede);
-    al_destroy_bitmap(jogo->chao);
     al_destroy_bitmap(jogo->agua);
     al_destroy_bitmap(jogo->cenario_1);
     al_destroy_bitmap(jogo->cenario_2);
